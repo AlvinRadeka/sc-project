6386c4403802f6218be2ef903581e4ec756fe569
alvinradeka <alvin.radeka@tokopedia.com>
2018-10-04 13:06:26 +0700

    html done. next: implement filter

--- db.go
+++ db.go
@@ -36,30 +36,99 @@ type ParsedUsers struct {
 	BirthDate   string
 	CreatedTime string
 	UpdatedTime string
+	Age         int
+	Calculation int
 }
 
 // HandleGet is
 func HandleGet() []ParsedUsers {
+	rawUsers := GetUsers()
 	users := []ParsedUsers{}
+	t := time.Now().Year()
+
+	for _, v := range rawUsers {
+		user := ParsedUsers{}
+		user.ID = v.ID
+		user.Name = v.Name
+		user.Msisdn = v.Msisdn
+		user.Email = v.Email
+		if v.BirthDate.Format("2006-01-02 15:04:05") != "0001-01-01 00:00:00" {
+			user.BirthDate = v.BirthDate.Format("2006-01-02")
+			user.Age = t - v.BirthDate.Year()
+		}
+		if v.CreatedTime.Format("2006-01-02 15:04:05") != "0001-01-01 00:00:00" {
+			user.CreatedTime = v.CreatedTime.Format("2006-01-02 15:04:05")
+		}
+		if v.UpdatedTime.Format("2006-01-02 15:04:05") != "0001-01-01 00:00:00" {
+			user.UpdatedTime = v.UpdatedTime.Format("2006-01-02 15:04:05")
+		}
+
+		users = append(users, user)
+	}
 
 	return users
 }
 
 // GetUsers is func for get all users
-func GetUsers() []Users {
+func GetUsers() []*Users {
 	fmt.Println("# Started Reading Users")
 
 	db, err := sqlx.Connect("postgres", "user=postgres password=root dbname=postgres sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	users := []Users{}
+
+	users := []*Users{}
 	queryString := `
-	SELECT * FROM sc_project.users
+	SELECT 
+		coalesce(id, '') as id,
+		coalesce(name, '') as name,
+		coalesce(msisdn, '') as msisdn,
+		coalesce(email, '') as email,
+		coalesce(birth_date, '0001-01-01 00:00:00') as birth_date,
+		coalesce(created_time, '0001-01-01 00:00:00') as created_time,
+		coalesce(updated_time, '0001-01-01 00:00:00') as updated_time
+	FROM sc_project.users
+	ORDER BY id
 	LIMIT 20
 	`
 
 	err = db.Select(&users, queryString)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("# Finished Reading Users")
+
+	return users
+}
+
+// GetFilteredUsers is
+func GetFilteredUsers(filter string) []*Users {
+	fmt.Println("# Started Reading Users")
+
+	db, err := sqlx.Connect("postgres", "user=postgres password=root dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	users := []*Users{}
+	queryString := `
+	SELECT 
+		coalesce(id, '') as id,
+		coalesce(name, '') as name,
+		coalesce(msisdn, '') as msisdn,
+		coalesce(email, '') as email,
+		coalesce(birth_date, '0001-01-01 00:00:00') as birth_date,
+		coalesce(created_time, '0001-01-01 00:00:00') as created_time,
+		coalesce(updated_time, '0001-01-01 00:00:00') as updated_time
+	FROM sc_project.users
+	WHERE name like '%$1%'
+	ORDER BY id
+	LIMIT 20
+	`
+
+	err = db.Select(&users, queryString, filter)
 	if err != nil {
 		fmt.Println(err)
 	}
--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-// Page is
-type Page struct {
-	Title string
-	Body  []byte
+// PageInfo is
+type PageInfo struct {
+	Users        []ParsedUsers
+	VisitorCount int
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	users := GetUsers()
+	// init get redis
+	visitorCount := 20
+	// up by 1, send to NSQ, and send to PageInfo
+	// ...
+
+	users := HandleGet()
+	PageInfo := PageInfo{
+		users,
+		visitorCount,
+	}
 	template, err := template.ParseFiles("page.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-	template.Execute(w, users)
+	template.Execute(w, PageInfo)
+}
+
+func filterHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Sukses masuk filter")
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/filter/", filterHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	//database.HandlePost()
 	//database.HandleGet()
